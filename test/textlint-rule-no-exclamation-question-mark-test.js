bf2b069dc5316034d24d5a7e8d1b078d5e9e1541
azu <azuciao@gmail.com>
2016-05-04 17:11:42 +0900

    initial commit

---
+++ test/textlint-rule-no-exclamation-question-mark-test.js
@@ -0,0 +1,91 @@
+// LICENSE : MIT
+"use strict";
+const TextLintTester = require("textlint-tester");
+const tester = new TextLintTester();
+import rule from "../src/textlint-rule-no-exclamation-question-mark";
+// ruleName, rule, { valid, invalid }
+tester.run("no-exclamation-question-mark", rule, {
+    valid: [
+        {
+            text: "text!?！？",
+            options: {
+                allowFullWidthExclamation: true,
+                // allow to use !
+                allowHalfWidthExclamation: true,
+                // allow to use ？
+                allowFullWidthQuestion: true,
+                // allow to use ?
+                allowHalfWidthQuestion: true
+            }
+        }
+    ],
+    invalid: [
+        // single match
+        {
+            text: "Hey!",
+            errors: [
+                {
+                    message: `Disallow to use "!".`,
+                    line: 1,
+                    column: 4
+                }
+            ]
+        },
+        // multiple match in multiple lines
+        {
+            text: "Hey!?\nHey！？",
+            errors: [
+                {
+                    message: `Disallow to use "!".`,
+                    line: 1,
+                    column: 4
+                },
+                {
+                    message: `Disallow to use "?".`,
+                    line: 1,
+                    column: 5
+                },
+                {
+                    message: `Disallow to use "！".`,
+                    line: 2,
+                    column: 4
+                },
+                {
+                    message: `Disallow to use "？".`,
+                    line: 2,
+                    column: 5
+                }
+            ]
+        },
+        // multiple hit items in a line
+        {
+            text: "Hey!?",
+            errors: [
+                {
+                    message: `Disallow to use "!".`,
+                    line: 1,
+                    column: 4
+                },
+                {
+                    message: `Disallow to use "?".`,
+                    line: 1,
+                    column: 5
+                }
+            ]
+        },
+        // Allow Option: !
+        {
+            text: "Hey!?",
+            options: {
+                allowHalfWidthExclamation: true
+            },
+            errors: [
+                {
+                    message: `Disallow to use "?".`,
+                    line: 1,
+                    column: 5
+                }
+            ]
+        }
+    ]
+});
